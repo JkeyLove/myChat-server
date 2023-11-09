f7c8b61819774800993be3dfe1308753e1ca2bea
JkeyLove <1826067520@qq.com>
2023-11-09 22:06:17 +0800

    websocket初始化

---
+++ src/main/java/com/example/chatserver/websocket/WebSocketServer.java
@@ -0,0 +1,73 @@
+package com.example.chatserver.websocket;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import javax.websocket.*;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+/**
+ * @author HFL
+ * @date 2022/5/16 15:17
+ * Websocket应用实现:
+ *  1.建立连接，连接放入连接池
+ *  2.关闭连接，连接移出连接池
+ *  3.接收客户端发送的消息，并做出相应处理
+ *  4.注入业务层的service
+ *  [注意：Spring管理的Bean是单例模式的，而WebSocket不是单例，注入时需要处理一下]
+ *  5.异常处理，连接移除连接池
+*/
+@Slf4j
+@Component
+@ServerEndpoint("/endPoint/{screen}")
+public class WebSocketServer {
+
+    /**
+     * 建立连接成功调用 (Session + 场景)
+     */
+    @OnOpen
+    public void onOpen(Session session,@PathParam("screen") String screen) throws IOException {
+        log.info("[onOpen][session({}) 接入, [screen: {}]", session, screen);
+        WebSocketServerPool.addDataConnect(session, screen);
+        //WebSocketServerPool.sendMessage(session, configurationScreenService.queryAllJsonById(screen));
+    }
+
+    /**
+     * 关闭连接时调用
+     * @param session 连接
+     */
+    @OnClose
+    public void onClose(Session session, CloseReason closeReason) {
+        log.info("[onClose][session({}) 连接关闭。关闭原因是({})}]", session, closeReason);
+        WebSocketServerPool.removeConnect(session);
+    }
+
+    /**
+     * 错误时调用
+     * @param session 连接
+     * @param throwable 异常
+     */
+    @OnError
+    public void onError(Session session, Throwable throwable) {
+        log.info("[onClose][session({}) 发生异常]", session, throwable);
+        WebSocketServerPool.removeConnect(session);
+    }
+
+    /**
+     * 收到客户端信息后，根据接收到的信息进行处理
+     * @param session 连接
+     * @param message 数据消息
+     */
+    @OnMessage
+    public void onMessage(Session session, String message) {
+        log.info("[onOpen][session({}) 接收到一条消息({})]", session, message);
+        // TODO: 2022/5/18 对于客户端发送的指令信息，解析后进行对应的逻辑处理
+        System.out.println(Arrays.toString(message.getBytes(StandardCharsets.UTF_8)));
+    }
+
+}
---
+++ src/main/java/com/example/chatserver/websocket/WebSocketServerPool.java
@@ -0,0 +1,76 @@
+package com.example.chatserver.websocket;
+
+import javax.websocket.Session;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+/**
+ * @author HFL
+ * @date 2022/5/16 9:39
+ * Websocket连接池、对连接池内连接操作 和数据推送方法
+ */
+public class WebSocketServerPool {
+
+    /**
+     * WebSocket连接池
+     */
+    private static ConcurrentMap<Session, String> dataConnect = new ConcurrentHashMap<>();
+
+    /**
+     * 将websocket连接，放入连接池
+     * @param session websocket连接
+     * @param screen 场景ID
+     */
+    public static void addDataConnect(Session session, String screen){
+        dataConnect.put(session, screen);
+        Iterator<Map.Entry<Session, String>> iterator = dataConnect.entrySet().iterator();
+        synchronized (iterator){
+            //移除失效连接
+            while(iterator.hasNext()){
+                Map.Entry<Session, String> entry = iterator.next();
+                Session sessionNew = entry.getKey();
+                Map<String, Object> userProperties = sessionNew.getUserProperties();
+                if(null != userProperties && null != userProperties.get("ReadyState") && "0" != String.valueOf(userProperties.get("ReadyState"))){
+                    iterator.remove();
+                }
+            }
+        }
+    }
+
+    /**
+     * 将websocket连接从连接池中移除
+     * @param session websocket连接
+     */
+    public static void removeConnect(Session session){
+        Iterator<Map.Entry<Session, String>> iterator = dataConnect.entrySet().iterator();
+        synchronized (iterator){
+            //主动移除连接
+            while (iterator.hasNext()){
+                if(session.equals(iterator.next().getKey())){
+                    iterator.remove();
+                }
+            }
+        }
+    }
+
+    /**
+     * 获取连接池中所有连接
+     * @return 连接池所有数据
+     */
+    public static ConcurrentMap<Session, String> getDataConnect(){
+        return dataConnect;
+    }
+
+    /**
+     * Websocket消息推送
+     * @param session 连接
+     * @param message 消息主体
+     * @throws IOException I/O异常
+     */
+    public static void sendMessage(Session session, String message) throws IOException {
+        session.getBasicRemote().sendText(message);
+    }
+}
